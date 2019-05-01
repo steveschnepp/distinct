3c4b813eb3b9c56dbd005186452af2d66fce48d5
Steve Schnepp <steve.schnepp@pwkf.org>
2019-05-01 14:35:31 +0200

    use url-safe Base64 instead of bespoke encoding
    This enables to have simpler parsing, and be opaque to attacks. And
    enables fixed-sized dirnames

--- distinct.pl
+++ distinct.pl
@@ -6,33 +6,36 @@ use warnings;
 use File::Find;
 use File::Temp qw/ tempdir /;
 use File::Path qw/ make_path /;
+use MIME::Base64 qw/ encode_base64url decode_base64url /;
 
 my $tmpdir = tempdir( CLEANUP => 1 );
 chdir($tmpdir);
 
+my $hashlen = 5;
+
 while (<>) {
-	s/\n$//;
-	s/\r$//;
-	# Replace _ by __
-	# replace / by _! so it is reversible
-	# replace . by _. so it is reversible
-	s/_/__/g;
-	s,/,_!,g;
-	s,\.,_.,g;
-	s, ,/,g;
-	make_path($_);
+	# using base64 from RFC 4648 §5, as it was done specifically for URLs
+	my $encoded = encode_base64url($_);
+
+	# Split the string to 5-chars ones
+	my @a = unpack("(a$hashlen)*", $encoded);
+
+	# Make it a path
+	my $path = join("/", @a);
+	make_path($path);
 }
 
 # Did read everything, collect
 sub process {
 	my $filename = $File::Find::name;
 	$filename =~ s,^\./,,;
-	$filename =~ s,/, ,g;
-	$filename =~ s,_\.,.,g;
-	$filename =~ s,_!,/,g;
-	$filename =~ s,__,_,g;
-	print "$filename";
-	print "\n";
+
+	# reassemble the base64
+	$filename =~ s,^\./,,;
+
+	my $decoded = decode_base64url($filename);
+
+	print $decoded;
 }
 
 find({ wanted => \&process, follow => 1 }, '.');
